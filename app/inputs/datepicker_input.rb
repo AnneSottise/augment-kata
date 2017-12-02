9d405895ae9714c79309962f8e5b63069bd6cbe6
Anne-Sophie Rouaux <annesophie.rouaux@gmail.com>
2017-12-02 20:10:16 +0100

    Inputs

---
+++ app/inputs/datepicker_input.rb
@@ -0,0 +1,22 @@
+class DatepickerInput < SimpleForm::Inputs::Base
+  def input(wrapper_options)
+    template.content_tag :div, class: 'input-group date_picker' do
+      date_field = @builder.text_field(attribute_name,
+                                       input_html_options)
+
+      span = template.content_tag :span, '', class: 'input-group-addon' do
+        template.content_tag :i, '', class: 'fa fa-calendar fa-fw'
+      end
+
+      date_field + span
+    end
+  end
+
+  def value
+    object.send(attribute_name) if object
+  end
+
+  def input_html_options
+    super.merge(class: 'col-md-4 form-control', value: value)
+  end
+end
